bd02a39ea96db7d0a4ff4331e40c601bf02c3371
Nikola Samardzic <nsamar@mit.edu>
2022-04-20 15:19:32 -0400

    Separate external and main-memory algorithms

---
+++ external.h
@@ -0,0 +1,49 @@
+#ifndef EXTERNAL_H_
+#define EXTERNAL_H_
+
+#include <iostream>
+#include <string>
+
+#include <stxxl/sort>
+#include <stxxl/vector>
+#include <stxxl/io>
+
+#include "constants.h"
+
+struct Cmp
+{
+ typedef int first_argument_type;
+ typedef int second_argument_type;
+ typedef bool result_type;
+ bool operator () (const int& a, const int& b) const
+ {
+ return a < b;
+ }
+ static int min_value()
+ {
+   return std::numeric_limits<int>::min();
+ }
+ static int max_value()
+ {
+  return std::numeric_limits<int>::max();
+ }
+};
+
+void AssertSorted(const stxxl::vector<int, 1, stxxl::lru_pager<8>, kBlockSize>& arr) {
+  std::cout << "vector size: " << arr.size() << std::endl;
+  for (uint64_t idx = 0; idx + 1 < arr.size(); ++idx) {
+    assert(arr[idx] <= arr[idx + 1]);
+  }
+}
+
+void SortExternal(const std::string& filename, int element_count) {
+  std::cout << "Creating vector..." << std::endl;
+  stxxl::syscall_file f(filename, stxxl::file::DIRECT | stxxl::file::RDWR);
+  stxxl::vector<int, 1, stxxl::lru_pager<8>, kBlockSize> v(&f, element_count);
+
+  std::cout << "Sorting..." << std::endl;
+  stxxl::sort(v.begin(), v.end(), Cmp(), kMemorySize);
+  AssertSorted(v);
+}
+
+#endif  // EXTERNAL_H_
---
+++ main_memory.h
@@ -0,0 +1,32 @@
+#ifndef MAIN_MEMORY_H_
+#define MAIN_MEMORY_H_
+
+#include "constants.h"
+
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <fstream>
+#include <iterator>
+
+template <class T>
+void AssertSorted(const std::vector<T>& arr) {
+  std::cout << "vector size: " << arr.size() << std::endl;
+  for (uint64_t idx = 0; idx + 1 < arr.size(); ++idx) {
+    assert(arr[idx] <= arr[idx + 1]);
+  }
+}
+
+void SortMainMemory(const std::string& filename, int element_count) {
+    std::cout << "Creating vector..." << std::endl;
+    std::ifstream fs(filename, std::ios::in | std::ios::binary);
+    std::vector<int> vec(element_count);
+    fs.read(reinterpret_cast<char*>(vec.data()), sizeof(int) * element_count);
+
+    std::cout << "Sorting..." << std::endl;
+    std::sort(vec.begin(), vec.end());
+    std::cout << "Checking..." << std::endl;
+    AssertSorted(vec);
+}
+
+#endif // MAIN_MEMORY_H_
