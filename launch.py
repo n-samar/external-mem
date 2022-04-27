35963ffef5a3b7c9ed00e72d1ff83af3272dac4e
Nikola Samardzic <nsamar@mit.edu>
2022-04-27 14:59:18 -0400

    [launch.py] Relocate iostat dump files to `data/`

--- launch.py
+++ launch.py
@@ -9,13 +9,14 @@ list_generate_data = [1]
 list_fraction_vertical = [0.01, 0.05, 0.5, 0.95, 0.99]
 list_use_external = [0, 1]
 
+os.system("mkdir data/")
 for log_element_count in list_log_element_count:
     for log_mem_size in list_log_mem_size:
         for generate_data in list_generate_data:
             for segment_width in list_segment_width:
                 for fraction_vertical in list_fraction_vertical:
                     for use_external in list_use_external:
-                        iostat_cmd = f"iostat -g ALL -x -t 1 | grep ALL > iostat_main_{log_element_count}_{log_mem_size}_{segment_width}_{generate_data}_{use_external}_{fraction_vertical}.txt &"
+                        iostat_cmd = f"iostat -g ALL -x -t 1 | grep ALL > data/iostat_main_{log_element_count}_{log_mem_size}_{segment_width}_{generate_data}_{use_external}_{fraction_vertical}.txt &"
                         os.system(iostat_cmd)
                         cmd = f"cgexec -g memory:6850 -- ./main {log_element_count} {log_mem_size} {segment_width} {generate_data} {use_external} {fraction_vertical}"
                         print(f"START: {cmd}")
